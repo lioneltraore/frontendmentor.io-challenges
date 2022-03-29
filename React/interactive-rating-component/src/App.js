bba07906385f89a0e12d6465ddc8871a027c3790
Lionel <admin@admins-MacBook-Pro.local>
2022-03-29 07:40:38 +0000

    add card component

--- React/interactive-rating-component/src/App.js
+++ React/interactive-rating-component/src/App.js
@@ -1,7 +1,10 @@
+import Card from "./components/card/Card";
+
 function App() {
   return (
-    <div>
-      <div className="attribution">
+    <div className="bg-darker flex flex-col items-center justify-center h-screen p-6 text-lighter">
+      <Card />
+      <div className="absolute bottom-3 inset-x-0">
         Challenge by{" "}
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
---
+++ React/interactive-rating-component/src/components/card/Card.js
@@ -0,0 +1,13 @@
+const Card = () => {
+  return (
+    <div>
+      <h2>How did we do?</h2>
+      <p>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+    </div>
+  );
+};
+
+export default Card;
