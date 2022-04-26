4750ca0c2c292185e655c0a0b7fdb9abafb4195b
Lionel Traore <hello@lioneltraore.com>
2022-04-26 07:40:07 +0000

    add colors

--- React/column-preview-card/tailwind.config.js
+++ React/column-preview-card/tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bright-orange': "#e38826",
+        'dark-cyan': "#006970",
+        'very-dark-cyan': "#004241",
+        'transparent': "#ffffffbf",
+        'light-gray': "#f2f2f2"
+      }
+    },
   },
   plugins: [],
 }
