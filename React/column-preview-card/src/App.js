e1c51c8242b7dcbd39a1a4734b89e174a9861896
Lionel Traore <hello@lioneltraore.com>
2022-05-21 11:59:19 +0000

    manage seo

--- React/column-preview-card/src/App.js
+++ React/column-preview-card/src/App.js
@@ -3,6 +3,7 @@ import CardSection from "./components/CardSection/CardSection";
 function App() {
   return (
     <div className="flex items-center justify-center px-6 py-20 overflow-y-scroll bg-white lg:h-screen">
+      <h1 className="hidden">3 Column Preview Card</h1>
       <CardSection />
     </div>
   );
