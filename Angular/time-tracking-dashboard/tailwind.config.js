13a05a9b93119ec8af1093c074e19dc63545eba0
Lionel Traore <hello@lioneltraore.com>
2022-07-31 00:10:45 +0000

    setup custom fonts

--- Angular/time-tracking-dashboard/tailwind.config.js
+++ Angular/time-tracking-dashboard/tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "sans": ["Rubik", "sans-serif"],
+      },
+      fontSize: {
+        "tiny": "15px",
+        "base": "18px",
+        "lg": "24px",
+        "xl": "32px",
+        "2xl": "40px",
+        "3xl": "56px",
+      },
+    },
   },
   plugins: [],
-}
+};
