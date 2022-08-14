2eb61cc0aa5bda549fe93096a2657d9a582a183f
Lionel Traore <hello@lioneltraore.com>
2022-08-14 22:38:25 +0000

    add report component

--- Angular/time-tracking-dashboard/src/app/app.module.ts
+++ Angular/time-tracking-dashboard/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReportComponent } from './report/report.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule
