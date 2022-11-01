9818feec91f0e9f9c761d08a5376b5935e535b54
Lionel Traore <hello@lioneltraore.com>
2022-11-01 10:15:21 +0000

    add desktop view

--- Static/product-preview-card-component/tailwind.config.js
+++ Static/product-preview-card-component/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-cyan": "#3C8067",
+        "cream": "#F2EBE3",
+        "v-dark-blue": "#1C232B",
+        "dark-g-blue": "#6C7289"
+      },
+      fontFamily: {
+        "sans": ["Montserrat", "sans-serif"],
+        "montserrat": ["Montserrat", "sans-serif"],
+        "fraunces": ["Fraunces"]
+      },
+    },
   },
   plugins: [],
 }
